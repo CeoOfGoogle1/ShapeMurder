b88b6db6645f2bbdcf00ae10c822c006b322d565
AmikMain <amikmain001@gmail.com>
2026-08-02 19:56:00 +0400

    Client Connection to Server and Data Transfer

---
+++ Assets/PlayerNameInput.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    private TMP_InputField nameInputField;
+
+    void Awake()
+    {
+        nameInputField = GetComponent<TMP_InputField>();    
+    }
+
+    private void OnEnable()
+    {
+        nameInputField.onValueChanged.AddListener(LocalDataManager.Instance.ChangeClientName);
+    }
+
+    private void OnDisable()
+    {
+        nameInputField.onValueChanged.RemoveListener(LocalDataManager.Instance.ChangeClientName);
+    }
+}
